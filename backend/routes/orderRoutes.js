cebdfd7d56e9988df86e6a9d5873470a8cebf9fb
KUNALL0ndhe <kunallondhex@gmail.com>
2024-04-21 15:17:16 +0530

    Backend endpoint done for updating product

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,11 +1,17 @@
 import express from 'express';
-import { createOrder, getOrderByID } from '../controllers/orderController.js';
+import { 
+    createOrder,
+    getOrderByID,
+    updateOrderToPaid,
+} from '../controllers/orderController.js';
 import { protect } from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
 
 router.route('/').post(protect, createOrder);
 router.route('/:id').get(protect, getOrderByID);
+router.route('/:id/pay').get(protect, updateOrderToPaid);
+
 
 
 export default router;
